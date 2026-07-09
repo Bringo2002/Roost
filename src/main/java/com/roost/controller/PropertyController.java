c5fdfc62a1b6c4259cb6860a39c13e044cdb711b
Brian Harrington <brianashoka@gmail.com>
2026-07-09 12:24:27 +0300

    Roost API live - Property CRUD with PostgreSQL

--- src/main/java/com/roost/controller/PropertyController.java
+++ src/main/java/com/roost/controller/PropertyController.java
@@ -3,13 +3,33 @@ package com.roost.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.roost.model.Property;
+import com.roost.service.PropertyService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/properties")
 public class PropertyController {
 
+    private final PropertyService propertyService;
+
+    public PropertyController(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
+    @GetMapping
+    public List<Property> getAllProperties() {
+        return propertyService.getAllProperties();
+    }
+
+    @PostMapping
+    public Property addProperty(@RequestBody Property property) {
+        return propertyService.addProperty(property);
+    }
+
     @GetMapping("/hello")
     public String hello() {
-        return "Roost API is live!";
+        return "Roost API is LIVE";
     }
 }
---
+++ src/main/java/com/roost/service/PropertyService.java
@@ -0,0 +1,24 @@
+package com.roost.service;
+
+import com.roost.model.Property;
+import com.roost.repository.PropertyRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PropertyService {
+
+    private final PropertyRepository propertyRepository;
+
+    public PropertyService(PropertyRepository propertyRepository) {
+        this.propertyRepository = propertyRepository;
+    }
+
+    public List<Property> getAllProperties() {
+        return propertyRepository.findAll();
+    }
+
+    public Property addProperty(Property property) {
+        return propertyRepository.save(property);
+    }
+}
